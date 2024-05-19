12079e32559d52219f334d2627d779fbfb8c5cc7
Max Kolle <maxkolle1@gmail.com>
2024-05-19 18:28:08 +0100

    Add working email functionality

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module caperture
 
 go 1.21.5
+
+require github.com/resend/resend-go/v2 v2.6.0 // indirect
--- main.go
+++ main.go
@@ -2,18 +2,30 @@ package main
 
 import (
   "log"
+  "fmt"
+  "os"
   "net/http"
+  "net/smtp"
   "html/template"
+
 )
 
 var templates map[string]*template.Template
+var gsmtpEmail string
+var gsmtpPassword string
+var gsmtpRecipient string
 
 func main() {
+  gsmtpEmail = os.Getenv("GSMTP_EMAIL")
+  gsmtpPassword = os.Getenv("GSMTP_PASSWORD")
+  gsmtpRecipient = os.Getenv("GSMTP_RECIPIENT")
+
   templates = make(map[string]*template.Template)
   templates["home"] = template.Must(template.ParseFiles("templates/base.html", "templates/home.html"))
   templates["contact"] = template.Must(template.ParseFiles("templates/base.html", "templates/contact.html"))
   templates["gallery"] = template.Must(template.ParseFiles("templates/base.html", "templates/gallery.html"))
   templates["thanks"] = template.Must(template.ParseFiles("templates/thanks.html"))
+
 
   http.HandleFunc("/", homeHandler)
   http.HandleFunc("/contact", contactHandler)
@@ -64,6 +76,8 @@ func contactHandler(w http.ResponseWriter, r *http.Request) {
       Details: details,
     }
 
+    go sendEmail(name, email, details)
+
     log.Printf("Contact form sent with following details: %v", submission)
     if err := templates["thanks"].Execute(w, submission); err != nil {
       http.Error(w, err.Error(), http.StatusInternalServerError)
@@ -97,4 +111,23 @@ func galleryHandler(w http.ResponseWriter, r *http.Request) {
   if err := templates["gallery"].ExecuteTemplate(w, "base.html", data); err != nil {
     http.Error(w, err.Error(), http.StatusInternalServerError)
   }
+}
+
+
+func sendEmail(name, customerEmail, details string) {
+  smtpHost := "smtp.gmail.com"
+  smtpPort := "587"
+
+  message := fmt.Sprintf("Subject: Gig Request from %s\r\n\r%s\n\nRequest sent from: %s", name, details, customerEmail)
+
+  auth := smtp.PlainAuth("", gsmtpEmail, gsmtpPassword, smtpHost)
+
+  err := smtp.SendMail(smtpHost+":"+smtpPort, auth, gsmtpEmail, []string{gsmtpRecipient}, []byte(message))
+
+  if err != nil {
+    log.Println("Failed to send email: ", err.Error())
+    return
+  }
+
+  log.Printf("Email sent to %s from customer %s via %s", gsmtpRecipient, customerEmail, gsmtpEmail)
 }
