71ab1b2be86d5f016e3ace34585150771d7e9e9b
Max Kolle <maxkolle1@gmail.com>
2024-07-19 20:24:15 +0100

    Add Photos Page (#1)
    * Add timeslot datetime field to form
    
    * Serve static files
    
    * Fix typo in README
    
    * Add deployment section to README
    
    * Add line to README
    
    * Add to README
    
    * Testing deployment
    
    * Use Bulma CSS and style contact form
    
    * Remove header and use navbar class
    
    * Add Bulma CSS to local server (remove CDN ref)
    
    * Update layouts of templates and structs of POST contact
    
    * Change subheading on home to button
    
    * Dynamically provide copyright year; Change wording
    
    * Add new photos page and b2 package (serve links to files as a list)
    
    * Update files templates
    
    * Add gitignore
    
    * Update README
    
    * Comment out Download All button

--- README.md
+++ README.md
@@ -9,5 +9,13 @@ This just uses a simple net/smtp call to send the email to a specified email add
 * `GSMTP_EMAIL` The email address with which the email shall be sent.
 * `GSMTP_RECIPIENT` The email address that should receive the email.
 * `GSMTP_PASSWORD` The app-specific password for the `GSMTP_EMAIL` account
+* `B2_KEY_ID` The numerical id of the API key associated with the Backblaze B2 Bucket
+* `B2_KEY` The secret key for that same Backblaze B2 API key.
 
-If any of these are undefined, an error will occur on the backend and will be logged (as of now, there are no persistent log files and errors are just written to the standard Logger output.) The customer-faing frontend currently just displays the success response fragment (thanks.html) irrespective of outcome.
+If any of these are undefined, an error will occur on the backend and will be logged (as of now, there are no persistent log files and errors are just written to the standard Logger output.) The customer-facing frontend currently just displays the success response fragment (thanks.html) irrespective of outcome.
+
+### Deployment
+Deployment is done by git pushing to the `deploy` remote repository. The post-receive hook should then fire off a bash script for building the project in /srv/tmp and moving it (on success) /src/www.
+
+NGINX is used as the server of choice to provide the application.
+Ideally, these are run as systemd daemons so that journald can log and systemd can restart should anything go wrong.
--- main.go
+++ main.go
@@ -1,35 +1,53 @@
 package main
 
 import (
+  "caperture/b2"
   "log"
   "fmt"
   "os"
   "net/http"
   "net/smtp"
   "html/template"
-
+  "time"
 )
 
 var templates map[string]*template.Template
 var gsmtpEmail string
 var gsmtpPassword string
 var gsmtpRecipient string
+var b2KeyID string
+var b2Key string
+const bucketName string = "jonathan-bain-caperture-photography"
+var now time.Time
+var currentYear int
 
 func main() {
+  now = time.Now()
+  currentYear, _, _ = now.Date()
+
   gsmtpEmail = os.Getenv("GSMTP_EMAIL")
   gsmtpPassword = os.Getenv("GSMTP_PASSWORD")
   gsmtpRecipient = os.Getenv("GSMTP_RECIPIENT")
+
+  b2KeyID = os.Getenv("B2_KEY_ID")
+  b2Key = os.Getenv("B2_KEY")
+
+  fs := http.FileServer(http.Dir("./static/"))
+
 
   templates = make(map[string]*template.Template)
   templates["home"] = template.Must(template.ParseFiles("templates/base.html", "templates/home.html"))
   templates["contact"] = template.Must(template.ParseFiles("templates/base.html", "templates/contact.html"))
   templates["gallery"] = template.Must(template.ParseFiles("templates/base.html", "templates/gallery.html"))
   templates["thanks"] = template.Must(template.ParseFiles("templates/thanks.html"))
+  templates["photos"] = template.Must(template.ParseFiles("templates/base.html", "templates/files.html"))
 
 
   http.HandleFunc("/", homeHandler)
   http.HandleFunc("/contact", contactHandler)
   http.HandleFunc("/gallery", galleryHandler)
+  http.HandleFunc("/photos", photosHandler)
+  http.Handle("/static/", http.StripPrefix("/static/", fs))
 
   log.Println("Starting the server on :8080")
   if err := http.ListenAndServe(":8080", nil); err != nil {
@@ -37,16 +55,24 @@ func main() {
   }
 }
 
+func Copyright() string {
+  return fmt.Sprintf("© %d Caperture Photography", currentYear)
+}
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
+
   data := struct {
     Title string
     Heading string
     Message string
+    SubMessage string
+    CopyRight string
   }{
     Title: "Home Page",
-    Heading: "Glasgow Photographer and Alumnus of The Garage Nightclub",
-    Message: "Who I am will go here.",
+    Heading: "Hi!",
+    Message: "I'm an experienced Glasgow based photographer.", 
+    SubMessage: "Let's talk photos!",
+    CopyRight: Copyright(),
   }
 
   if err := templates["home"].ExecuteTemplate(w, "base.html", data); err != nil {
@@ -65,18 +91,21 @@ func contactHandler(w http.ResponseWriter, r *http.Request) {
     name := r.Form.Get("name")
     email := r.Form.Get("email")
     details := r.Form.Get("details")
+    phonenumber := r.Form.Get("phonenumber")
 
     submission := struct{
       Name string
       Email string
       Details string
+      PhoneNumber string
     }{
       Name: name,
       Email: email,
       Details: details,
+      PhoneNumber: phonenumber,
     }
 
-    go sendEmail(name, email, details)
+    go sendEmail(name, email, details, phonenumber)
 
     log.Printf("Contact form sent with following details: %v", submission)
     if err := templates["thanks"].Execute(w, submission); err != nil {
@@ -87,9 +116,13 @@ func contactHandler(w http.ResponseWriter, r *http.Request) {
     data := struct {
       Title string
       Heading string
+      SubHeading string
+      CopyRight string
     }{
       Title: "Contact Page",
-      Heading: "Contact me using this form and I will get back to you as soon as I can.",
+      Heading: "Contact",
+      SubHeading: "Contact me using this form and I will get back to you as soon as I can.",
+      CopyRight: Copyright(),
     }
 
     if err := templates["contact"].ExecuteTemplate(w, "base.html", data); err != nil {
@@ -102,9 +135,11 @@ func galleryHandler(w http.ResponseWriter, r *http.Request) {
   data := struct {
     Title string
     Heading string
+    CopyRight string
   }{
     Title: "Gallery Page",
     Heading: "Place some photos here and we should be good to go. Likely just link to B2",
+    CopyRight: Copyright(),
   }
 
 
@@ -114,11 +149,37 @@ func galleryHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 
-func sendEmail(name, customerEmail, details string) {
+func photosHandler(w http.ResponseWriter, r *http.Request) {
+  links, err := b2.GetBucketFiles(b2KeyID, b2Key, bucketName)
+  if err != nil {
+    log.Println(err)
+    http.Error(w, err.Error(), http.StatusInternalServerError)
+    return
+  }
+
+  data := struct {
+    Title string
+    Heading string
+    CopyRight string
+    Items []b2.Link
+  }{
+    Title: "Photos Page",
+    Heading: "Here are photos from the gig. Make sure to download the file and post it as these links are temporary!",
+    CopyRight: Copyright(),
+    Items: links,
+  }
+
+  if err := templates["photos"].ExecuteTemplate(w, "base.html", data); err != nil {
+    http.Error(w, err.Error(), http.StatusInternalServerError)
+  }
+}
+
+
+func sendEmail(name, customerEmail, details, phonenumber string) {
   smtpHost := "smtp.gmail.com"
   smtpPort := "587"
 
-  message := fmt.Sprintf("Subject: Gig Request from %s\r\n\r%s\n\nRequest sent from: %s", name, details, customerEmail)
+  message := fmt.Sprintf("Subject: Gig Request from %s\r\n\r%s\n\nRequest sent from: %s\nTel: (+44) 0%s", name, details, customerEmail, phonenumber)
 
   auth := smtp.PlainAuth("", gsmtpEmail, gsmtpPassword, smtpHost)
 
